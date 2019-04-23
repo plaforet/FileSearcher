42baf6a96fb2c747181d3edff38758e9dbd01c21
PLAFORET <PLAFORET@E04585CG739146D.AD.HISD.ORG>
2019-04-23 10:56:21 -0500

    Separate out methods used to create files.

--- src/Runner.java
+++ src/Runner.java
@@ -31,62 +31,6 @@ public class Runner {
 
 	}
 
-	public static void wordList(File list) throws FileNotFoundException {
-		Scanner words = new Scanner(list);
-		while (words.hasNextLine()) {
-			wordsList.add(words.nextLine());
-		}
-		words.close();
-
-	}
-
-	public static void makeFile(File folder) throws IOException {
-		int r = rand.nextInt(wordsList.size());
-		File temp = new File(folder, wordsList.get(r) + ".txt");
-		temp.createNewFile();
-	}
-
-	public static void fillFolders(File folder) throws IOException {
-		File[] fileList;
-		fileList = folder.listFiles();
-		for (int i = 0; i < fileList.length; i++) {
-			if (fileList[i].isDirectory()) {
-				makeFile(fileList[i]);
-				fillFolders(fileList[i]);
-			}
-		}
-
-	}
-
-	public static void fillFiles(File folder) throws IOException {
-		File[] fileList;
-		fileList = folder.listFiles();
-		for (int i = 0; i < fileList.length; i++) {
-			if (fileList[i].isDirectory()) {
-				fillFiles(fileList[i]);
-			}
-
-			else {
-				writeToFile(fileList[i], fileList[i].getPath());
-			}
-		}
-
-	}
-
-	public static void writeToFile(File file, String path) {
-		try {
-			PrintWriter outputStream = new PrintWriter(path);
-			String text = "";
-			for (int i = 0; i < rand.nextInt(50); i++) {
-				text += wordsList.get(rand.nextInt(listLength)) + " ";
-			}
-			outputStream.println(text);
-			outputStream.close();
-		} catch (FileNotFoundException e) {
-
-		}
-	}
-
 	// Example of student code
 	public static boolean searchFolder(File folder, String search) {
 		for (File file : folder.listFiles()) {
