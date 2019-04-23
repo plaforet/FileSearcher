8ae0b1f4bef64c66ae4389fd804b53ea513b92a9
plaforet <plaforet@houstonisd.org>
2019-04-23 14:52:47 -0500

    Fixed all the things!

--- src/Runner.java
+++ src/Runner.java
@@ -11,6 +11,62 @@ public class Runner {
 	static Random rand = new Random();
 	static int listLength;
 
+	public static void wordList(File list) throws FileNotFoundException {
+		Scanner words = new Scanner(list);
+		while (words.hasNextLine()) {
+			wordsList.add(words.nextLine());
+		}
+		words.close();
+
+	}
+
+	public static void makeFile(File folder) throws IOException {
+		int r = rand.nextInt(wordsList.size());
+		File temp = new File(folder, wordsList.get(r) + ".txt");
+		temp.createNewFile();
+	}
+
+	public static void writeToFile(File file, String path) {
+		try {
+			PrintWriter outputStream = new PrintWriter(path);
+			String text = "";
+			for (int i = 0; i < rand.nextInt(50); i++) {
+				text += wordsList.get(rand.nextInt(listLength)) + " ";
+			}
+			outputStream.println(text);
+			outputStream.close();
+		} catch (FileNotFoundException e) {
+
+		}
+	}
+
+	public static void fillFiles(File folder) throws IOException {
+		File[] fileList;
+		fileList = folder.listFiles();
+		for (int i = 0; i < fileList.length; i++) {
+			if (fileList[i].isDirectory()) {
+				fillFiles(fileList[i]);
+			}
+
+			else {
+				writeToFile(fileList[i], fileList[i].getPath());
+			}
+		}
+
+	}
+
+	public static void fillFolders(File folder) throws IOException {
+		File[] fileList;
+		fileList = folder.listFiles();
+		for (int i = 0; i < fileList.length; i++) {
+			if (fileList[i].isDirectory()) {
+				makeFile(fileList[i]);
+				fillFolders(fileList[i]);
+			}
+		}
+
+	}
+
 	// Starter code
 	public static boolean searchFile(File f, String s) {
 		Scanner fileText;
@@ -25,7 +81,7 @@ public class Runner {
 			fileText.close();
 			return false;
 		} catch (FileNotFoundException e) {
-			System.out.println("File Not Found.");
+			System.out.println("File Not Found: " + f.getPath());
 			return false;
 		}
 
@@ -79,12 +135,12 @@ public class Runner {
 		listLength = wordsList.size();
 		// fillFolders(new File("Files"));
 		// fillFiles(new File("Files"));
-		File root = new File("Files/");
+		File root = new File("files/");
 		File usedWordsFile = new File("usedWords.txt");
 		PrintWriter outputStream = new PrintWriter(usedWordsFile);
 		usedWords(root, outputStream);
 		outputStream.close();
-		System.out.println(searchFile(new File("Files/test.txt"), "bike"));
+		System.out.println(searchFile(new File("test.txt"), "bike"));
 	}
 
 }
